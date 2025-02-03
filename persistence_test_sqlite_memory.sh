5f62d91b44df4d3879de394e8b40747f92d0f740
Alessio <alex@playfulpachyderm.com>
2025-02-02 20:35:46 -0800

    Update CHANGELOG.txt for v0.6.22

--- persistence_test_sqlite_memory.sh
+++ persistence_test_sqlite_memory.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo mount -t tmpfs -o size=100M tmpfs pkg/persistence/test_profiles
+sudo mount -t tmpfs -o size=100M tmpfs cmd/data
+sudo mount -t tmpfs -o size=1000M tmpfs sample_data/profile
